febbe5b7e4d43d840b88ec2cfbd360659fb5194d
Jaqueline Fernandes <dhelly@gmail.com>
2019-07-25 17:34:41 -0300

    script inutil

---
+++ Youtube sem propaganda.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Youtube sem propaganda
+// @namespace    inutil
+// @version      0.1
+// @description  try to take over the world!
+// @author       inutil
+// @include      https://www.youtube.com/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @grant        GM_log
+// ==/UserScript==
+
+(function(){
+    'use strict';
+    var $ = window.jQuery;
+    console.log('Iniciando o script');
+    var myVar = setInterval(press, 1000);
+
+        function press() {
+            <!-- console.log('verificando'); !>
+            var botao = $('.ytp-ad-skip-button-text');
+            if(botao !== null){
+                $('.ytp-ad-skip-button-text').click();
+            }
+        }
+
+        function stopClick() {
+            console.log('Encerrando');
+            clearInterval(myVar);
+        }
+        <!-- var inputStart=document.createElement("input"); !>
+        var inputStop=document.createElement("input");
+        <!-- inputStart.type="button"; !>
+        inputStop.type="button";
+        <!-- inputStart.value="Start"; !>
+        inputStop.value="Ver Inutil";
+        <!-- inputStart.onclick = press; !>
+        inputStop.onclick = stopClick;
+        var pos = document.getElementById('container');
+        <!-- pos.appendChild(inputStart); !>
+        pos.appendChild(inputStop);
+}());
